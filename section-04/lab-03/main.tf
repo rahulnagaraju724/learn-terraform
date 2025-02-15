b34f55829978ab99b7862f5e14ac15d836ca52ac
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-14 20:11:26 -0600

    added aws subnet

--- section-04/lab-03/main.tf
+++ section-04/lab-03/main.tf
@@ -143,3 +143,15 @@ resource "aws_instance" "web_server" {
     Name = "Ubuntu EC2 Server"
   }
 }
+
+resource "aws_subnet" "variables-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.250.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "sub-variables-us-east-1a"
+    Terraform = "true"
+  }
+}
