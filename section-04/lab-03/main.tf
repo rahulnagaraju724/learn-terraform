e97029bdf69cbc0e823f05ab67872e15c9ce8c01
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-15 01:20:52 -0600

    added modules

--- section-04/lab-03/main.tf
+++ section-04/lab-03/main.tf
@@ -177,3 +177,23 @@ resource "aws_subnet" "variables-subnet" {
   }
 }
 
+ module "subnet_addrs" {
+  source  = "hashicorp/subnets/cidr"
+  version = "1.0.0"
+  
+  base_cidr_block = "10.0.0.0/22"
+  networks = [
+  {
+    name     = "module_network_a"
+    new_bits = 2
+  },
+  {
+    name     = "module_network_b"
+    new_bits = 2
+  },
+ ]
+}
+
+output "subnet_addrs" {
+  value = module.subnet_addrs.network_cidr_blocks
+}
