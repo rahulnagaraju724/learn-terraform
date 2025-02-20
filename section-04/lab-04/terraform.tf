332d91ecfa76ded798d2e3a6215b3008459fc47f
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-19 20:45:46 -0600

    added new providers

--- section-04/lab-04/terraform.tf
+++ section-04/lab-04/terraform.tf
@@ -1,8 +1,16 @@
 terraform{
+    required_version = ">= 1.0.0"
     required_providers{
+        aws = {
+            source  = "hashicorp/aws"
+        }
         http={
             source = "hashicorp/http"
             version = "3.4.5"
         }
+        random = {
+            source = "hashicorp/random"
+            version = "3.1.0"
+        }       
     }
 }
