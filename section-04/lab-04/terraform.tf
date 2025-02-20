70a57480323f4bb9541702cbf86c0748e3f577e7
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-19 20:48:50 -0600

    added tls provider

--- section-04/lab-04/terraform.tf
+++ section-04/lab-04/terraform.tf
@@ -15,6 +15,10 @@ terraform{
         local = {
             source  = "hashicorp/local"
             version = "2.1.0"
-        }       
+        } 
+        tls = {
+            source  = "hashicorp/tls"
+            version = "3.1.0"
+        }      
     }
 }
