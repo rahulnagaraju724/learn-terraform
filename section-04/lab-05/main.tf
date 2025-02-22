b03c954037318976b40737dd51c4e25b24dd7340
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-22 14:18:18 -0600

    added gateways

--- section-04/lab-05/main.tf
+++ section-04/lab-05/main.tf
@@ -46,3 +46,32 @@ resource "aws_subnet" "public_subnets" {
     Terraform = "true"
   }
 }
+
+#Create Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "demo_igw"
+  }
+}
+
+#Create EIP for NAT Gateway
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "demo_igw_eip"
+  }
+}
+
+
+#Create NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "demo_nat_gateway"
+  }
+}
+
