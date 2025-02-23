082c7f54e9515839d8fb222ffe465dad00de895d
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-23 11:35:52 -0600

    update size in module block of root main.tf

--- section-06/lab-05/main.tf
+++ section-06/lab-05/main.tf
@@ -285,6 +285,7 @@ module "server" {
   source          = "./modules/server"
   ami             = data.aws_ami.ubuntu.id
   subnet_id       = aws_subnet.public_subnets["public_subnet_3"].id
+  size            = "t3.micro"
   security_groups = [
     aws_security_group.vpc-ping.id,
     aws_security_group.ingress-ssh.id,
