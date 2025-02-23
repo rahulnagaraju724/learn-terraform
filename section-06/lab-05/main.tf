537e140581a461aa365a9533cac47e35fd4acaf0
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-23 11:12:57 -0600

    add public module

--- section-06/lab-05/main.tf
+++ section-06/lab-05/main.tf
@@ -318,3 +318,31 @@ output "public_ip_server_subnet_1" {
 output "public_dns_server_subnet_1" {
   value = module.server_subnet_1.public_dns
 }
+
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "4.9.0"
+
+  # Autoscaling group
+  name = "myasg"
+
+  vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id, 
+  aws_subnet.private_subnets["private_subnet_2"].id, 
+  aws_subnet.private_subnets["private_subnet_3"].id]
+  min_size            = 0
+  max_size            = 1
+  desired_capacity    = 1
+
+  # Launch template
+  use_lt    = true
+  create_lt = true
+
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags_as_map = {
+    Name = "Web EC2 Server 2"
+  }
+
+}
