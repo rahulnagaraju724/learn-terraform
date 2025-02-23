743193d9c744176cb491e0bd53dd2f2adbf1c7e1
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-23 11:25:03 -0600

    update source

--- section-06/lab-05/main.tf
+++ section-06/lab-05/main.tf
@@ -321,7 +321,7 @@ output "public_dns_server_subnet_1" {
 
 
 module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
+  source  = "https://github.com/terraform-aws-modules/terraform-aws-autoscaling"
   version = "8.1.0"
 
   # Autoscaling group
