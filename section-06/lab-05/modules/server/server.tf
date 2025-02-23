2bf1d361707d2615af8b15ab824ea4ebe43c443f
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-23 11:31:58 -0600

    re arrange input, variables and server code in module

--- section-06/lab-05/modules/server/server.tf
+++ section-06/lab-05/modules/server/server.tf
@@ -1,11 +1,3 @@
-variable "ami" {}
-variable "size" {
-  default = "t3.micro"
-}
-variable "subnet_id" {}
-variable "security_groups" {
-  type = list(any)
-}
 resource "aws_instance" "web" {
   ami                    = var.ami
   instance_type          = var.size
