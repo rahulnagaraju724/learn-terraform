06b115541a76840935ba7e685dd350f5fc0487e8
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-26 20:34:29 -0600

    add new random string

--- section-11/lab-08/main.tf
+++ section-11/lab-08/main.tf
@@ -9,3 +9,16 @@ min_upper = 3
 output "random_string"{
   value=random_string.random.result
 }
+
+
+resource "random_string" "random2"    {
+  length = 20
+  special = true
+min_numeric = 17
+      min_special = 1
+min_upper = 2
+}
+
+output "random_string_2"{
+  value=random_string.rahul.result
+}
